91487088caf4c7c9a851776ffede5a705b01e892
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-14 20:11:38 -0500

    add missing file and fix bug when image is deleted

--- frontend/scripts/components/shopper_page.js
+++ frontend/scripts/components/shopper_page.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ImageUploader from 'react-images-upload';
+import {generateBase64FromImage} from "../utils/image_parser"
 
 class ShopperPage extends React.Component{
     constructor(props){
@@ -11,7 +12,7 @@ class ShopperPage extends React.Component{
     }
 
     ImageChangeHandler = (files) => {
-        if (files) {
+        if (files.length>0) {
           generateBase64FromImage(files[0])
             .then(b64 => {
               this.setState({ image: b64 });
@@ -28,11 +29,13 @@ class ShopperPage extends React.Component{
         return (
         <>
             <ImageUploader
+                withPreview={true}
                 withIcon={true}
                 buttonText='Choose images'
                 onChange={this.ImageChangeHandler.bind(this)}
                 imgExtension={['.jpg', '.gif', '.png', '.gif']}
                 maxFileSize={5242880}
+                singleImage={true}
             />
         </>
         );
